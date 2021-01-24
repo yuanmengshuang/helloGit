7b3af9e3a0e487f848da5a7568c6b1c814ff12a4
yuanmengshuang <1964977105@qq.com>
2021-01-24 10:23:21 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @ClassName : HelloGit
+ * @Author : yms
+ * @Date: 2021/1/24 10:15
+ * @Description :
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
